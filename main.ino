1c2a4b8c1961b76d4031639dd74183a6636970a1
nodaSnowball <119981900+nodaSnowball@users.noreply.github.com>
2023-02-28 13:05:21 -0500

    Update main.ino

--- main.ino
+++ main.ino
@@ -5,18 +5,6 @@
 long duration;
 int distance;
 
-double getdistance(){
-  digitalWrite(trigpin,LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigpin,HIGH);
-  delayMicroseconds(10);
-  digitalWrite(LOW);
-  duration=pulseIn(echopin,HIGH);
-  distance=duration*0.034/2;
-  Serial.print('Distance=');
-  Serial.println(distance);
-  return distance;
-}
 void setup(){
   pinMode(trigpin,OUTPUT);
   pinMode(echopin,INPUT);
