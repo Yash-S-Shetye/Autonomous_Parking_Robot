64cca81806f033d5b2f4b29ae64b3923b695af38
Yash Shetye <87604378+Yash-S-Shetye@users.noreply.github.com>
2023-03-03 18:02:42 -0500

    Update servo_motors_test.ino

--- servo_motors_test.ino
+++ servo_motors_test.ino
@@ -1,30 +1,30 @@
-#include 
-Servo myservo1, myservo2; 
+#include <Servo.h>
+Servo servoLeft, servoRight; 
 int pos = 0;
 void setup() {
   // The servo control wire is connected to Arduino D2 and D4 pin.
-  myservo1.attach(2);
-  myservo2.attach(4);
-  // Servo is stationary.
-  myservo1.write(90);
-  myservo2.write(90);
+  servoLeft.attach(2);
+  servoRight.attach(4);
+  
+  servoLeft.write(750);
+  servoRight.write(750);
 }
 
 void loop() {
   // Both servos spin forward at full speed for 1 second.
-  myservo.write(180);
-  myservo.write(180);
+  servoLeft.write(1300);
+  servoLeft.write(1700);
   delay(1000);
   // Both servos are stationary for 1 second.
-  myservo.write(90);
-  myservo.write(90);
+  servoLeft.write(750);
+  servoRight.write(750);
   delay(1000);
   // Both servos spin in reverse at full speed for 1 second.
-  myservo.write(0);
-  myservo.write(0);
+  servoLeft.write(1700);
+  servoRight.write(1300);
   delay(1000);
   // Both servos are stationary for 1 second.
-  myservo.write(90);
-  myservo.write(90);
+  servoLeft.write(750);
+  servoRight.write(750);
   delay(1000);
 }
