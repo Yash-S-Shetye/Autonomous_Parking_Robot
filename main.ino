5afc0c2d0b04757a1814f8e30483a044f44b5851
Yash Shetye <87604378+Yash-S-Shetye@users.noreply.github.com>
2023-03-04 16:07:53 -0500

    Update main.ino

--- main.ino
+++ main.ino
@@ -2,8 +2,15 @@
 #define echopin 10
 #define leftwheel 5
 #define rightwheel 6
-long duration;
-int distance;
+
+class Robot {
+  private:
+  long duration;
+  int distance;
+  
+  public:
+  void getDistance();
+};
 
 void setup(){
   pinMode(trigpin,OUTPUT);
