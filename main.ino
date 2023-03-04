9d8a79ed572024e8cdb296beda41834f48de94e7
Yash Shetye <87604378+Yash-S-Shetye@users.noreply.github.com>
2023-03-04 16:15:23 -0500

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,23 +1,46 @@
+//Defining all the pins for sensors and wheels
 #define trigpin 9
 #define echopin 10
 #define leftwheel 5
 #define rightwheel 6
-
+//Creating a main class
 class Robot {
   private:
+  //Defining required variables
   long duration;
   int distance;
   
   public:
+  //Declaring all the required functions
   void getDistance();
+  void driveRobot();
+  void displayData();
 };
 
-void setup(){
+//Defining getDistance function
+void Robot::getDistance() {
+  
+}
+
+//Defining driveRobot function
+void Robot::driveRobot() {
+  
+}
+
+//Defining displayData function
+void Robot::displayData() {
+  
+}
+
+
+void setup() {
+  //Creating objects of Robot class and calling all the functions using the created object
+  Robot obj;
   pinMode(trigpin,OUTPUT);
   pinMode(echopin,INPUT);
   Serial.begin(9600);
 }
 
-void loop(){
+void loop() {
 
 }
