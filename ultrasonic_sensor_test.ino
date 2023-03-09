f303d4a8abce904f12a99c82f04d9e60e0465770
Yash Shetye <87604378+Yash-S-Shetye@users.noreply.github.com>
2023-03-09 16:44:56 -0500

    Update ultrasonic_sensor_test.ino

--- ultrasonic_sensor_test.ino
+++ ultrasonic_sensor_test.ino
@@ -1,27 +1,26 @@
-// define hardware connecction pins
-const int trigPin = 9;
-const int echoPin = 10;
-// defines variables
-long duration;
-int distance;
+const int SigPin = 8;
+float invcmCosnt = (2*1000000)/(100*344.8); //cmDist=rawTime/invcmCosnt
+float invinCosnt = invcmCosnt*2.54; //inDist=rawTime/invinCosnt
 void setup() {
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-  Serial.begin(9600); // Starts the serial communication
+Serial.begin(9600);
 }
 void loop() {
-  // Clears the trigPin
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
-  // Calculating the distance
-  distance = duration * 0.034 / 2;
-  // Prints the distance on the Serial Monitor
-  Serial.print("Distance: ");
-  Serial.println(distance);
+float rawTime, inDist, inDist1, cmDist, cmDist1; //declare variables
+/* Ping))) produces chirp when a HIGH pulse of 2us (min) or longer duration (5us typical) is
+applied to Sig pin. To have a clarn HIGH pulse, give a low and then high */
+pinMode(SigPin, OUTPUT);
+digitalWrite(SigPin, LOW);
+delayMicroseconds(2);
+digitalWrite(SigPin, HIGH);
+delayMicroseconds(5);
+digitalWrite(SigPin, LOW);
+/* Now same Sign pin is to be used to monitor if the return echo has been received. Essentially,
+Sig pin remains HIGH until a return echo is received and at that time it goes LOW. So measure
+the time duration for HIGH at the Sig pin */
+pinMode(SigPin, INPUT);
+rawTime = pulseIn(SigPin, HIGH); //measured in u-seconds
+cmDist = rawTime/invcmCosnt;
+Serial.print(cmDist);
+Serial.println("cm, ");
+delay(1000);
 }
