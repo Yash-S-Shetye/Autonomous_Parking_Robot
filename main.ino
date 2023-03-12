5de2a907f4dc3ee49e3220dad6ae417264060fdf
nodaSnowball <119981900+nodaSnowball@users.noreply.github.com>
2023-03-12 16:39:43 -0400

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,21 +1,28 @@
 //Defining all the pins for sensors and wheels
-#define trigpin 9
-#define echopin 10
-#define leftwheel 5
-#define rightwheel 6
 //Creating a main class
 class Robot {
   private:
   //Defining required variables
   long duration;
   int distance;
+  const int leftwheelpin=5;
+  const rightwheelpin=6
+  int ultrasonic=9;
+  Servo servoLeft, servoRight;
   
   public:
+  Robot();
   //Declaring all the required functions
   void getDistance();
   void driveRobot();
   void displayData();
 };
+
+//Initialization
+Robot::Robot(){
+  servoLeft.attach(leftwheelpin);
+  servoRight.attach(rightwheelpin);  
+}
 
 //Defining getDistance function
 void Robot::getDistance() {
@@ -23,8 +30,16 @@ void Robot::getDistance() {
 }
 
 //Defining driveRobot function
-void Robot::driveRobot() {
-  
+void Robot::driveRobot(char i) {
+  switch(i){
+  // f, b, l, r, s means forward, backward, left, right, and stop
+    case 'f':servoLeft.write(1550);servoRight.write(1450);break;
+    case 'b':servoLeft.write(1450);servoRight.write(1550);break;
+    case 'l':servoLeft.write(1550);servoRight.write(1500);break;
+    case 'r':servoLeft.write(1500);servoRight.write(1550);break;
+    case 's':servoLeft.write(1500);servoRight.write(1500);break;
+    default:Serial.println("Unclear command for motors");break;
+  }
 }
 
 //Defining displayData function
@@ -35,12 +50,10 @@ void Robot::displayData() {
 
 void setup() {
   //Creating objects of Robot class and calling all the functions using the created object
-  Robot obj;
-  pinMode(trigpin,OUTPUT);
-  pinMode(echopin,INPUT);
   Serial.begin(9600);
+  Robot rob;
 }
 
 void loop() {
-
+ 
 }
