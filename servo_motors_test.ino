96f6354c2a362fd612cc1b87c8299a8497b3c7bf
Yash Shetye <87604378+Yash-S-Shetye@users.noreply.github.com>
2023-03-13 16:36:47 -0400

    Update servo_motors_test.ino

--- servo_motors_test.ino
+++ servo_motors_test.ino
@@ -4,26 +4,46 @@ int pos = 0;
 void setup() {
   // The servo control wire is connected to Arduino D2 and D4 pin.
   servoLeft.attach(2);
-  servoRight.attach(4);
+  servoRight.attach(3);
 }
 
 void loop() {
-  // Left wheel servo spin forward while right wheel servo spins backward at full speed for 1 second
-  servoLeft.write(1700);
-  servoRight.write(1300);
+  // Forward
+  servoLeft.write(1550);
+  servoRight.write(1450);
   delay(1000);
 
-  // Both wheel servos stop for 1 second
+  // Stop
   servoLeft.write(1500);
   servoRight.write(1500);
   delay(1000);
   
-  // Right wheel servo spin forward while left wheel servo spins backward at full speed for 1 second
-  servoLeft.write(1300);
-  servoRight.write(1700);
+  // Backward
+  servoLeft.write(1450);
+  servoRight.write(1550);
   delay(1000);
 
-  // Both wheel servos stop for 1 second
+  // Stop
+  servoLeft.write(1500);
+  servoRight.write(1500);
+  delay(1000);
+
+  // Left
+  servoLeft.write(1450);
+  servoRight.write(1450);
+  delay(1000);
+
+  // Stop
+  servoLeft.write(1500);
+  servoRight.write(1500);
+  delay(1000);
+  
+  // Right
+  servoLeft.write(1550);
+  servoRight.write(1550);
+  delay(1000);
+
+  // Stop
   servoLeft.write(1500);
   servoRight.write(1500);
   delay(1000);
