280a6e3c51ef86390e2e30968ea721dfe5fd8c63
Yash Shetye <87604378+Yash-S-Shetye@users.noreply.github.com>
2023-03-22 18:12:56 -0400

    Update main.ino

--- main.ino
+++ main.ino
@@ -2,15 +2,17 @@
 #include <SoftwareSerial.h>
 #define black 1
 #define white 0
-#define obsdistance 15
-#define nighty 800
-#define oneeighty 1800
+#define obsdistance 20
+#define nighty 1000
+#define oneeighty 2000
 
 bool finish=false;
 bool localfinish=false;
 int c_intersection=0;
 int parkinglot[8]={0,0,0,0,0,0,0,0};
 int parkidx=0;
+int obstacles_no=0;
+int c_tw=0;
 
 //Creating a robot class containing all the functions will be used
 class Robot {
@@ -18,11 +20,11 @@ class Robot {
   //Defining required variables
   const int leftwheel=2;
   const int rightwheel=3;
-  const int ultrasonic=9;
+  const int ultrasonic=11;
   const int TxPin = 12;
   const int IR_ML=4;
   const int IR_MR=8;
-  const int led=13;
+  const int led=7;
   Servo servoLeft, servoRight;
   
   
@@ -67,10 +69,11 @@ bool Robot::isobstacle() {
   
   
   if (distance<obsdistance){
-    digitalWrite(led,HIGH);delay(10);
+    digitalWrite(led,HIGH);delay(50);
+    digitalWrite(led,LOW);delay(50);
     return true;}
   else{
-    digitalWrite(led,LOW);delay(10);
+    //digitalWrite(led,LOW);delay(10);
     return false;}
 }
 
@@ -117,9 +120,10 @@ void Robot::lcd_display(char disp) {
   
   switch(disp) {
     case 'i': mySerial.print("Intersection");mySerial.write(13);mySerial.print("Detected");delay(500);mySerial.write(12);break; // i - Intersection detected
-    case 'o': mySerial.print("Object Detected");delay(500);mySerial.write(12);break; // o - object detected
-    case 'p': mySerial.print("Parked");mySerial.write(13);mySerial.print("Successfully !!");delay(500);mySerial.write(12);break; // p - parked successfully
-    case 's': mySerial.print("Occupied Spaces");mySerial.write(13);mySerial.print(parkidx);delay(500);mySerial.write(12);break; // s - Number of occupied spaces
+    case 'p': mySerial.print("Parked");mySerial.write(13);mySerial.print("Successfully !!");delay(3000);mySerial.write(12);break; // p - parked successfully
+    case 's': mySerial.print("Occupied Spaces");mySerial.write(13);mySerial.print(obstacles_no);delay(3000);mySerial.write(12);break; // s - Number of occupied spaces
+    case 'f': mySerial.print("Four Wheelers");mySerial.write(13);mySerial.print(obstacles_no-c_tw);delay(3000);mySerial.write(12);break; // s - Number of occupied spaces
+    case 't': mySerial.print("Two Wheelers");mySerial.write(13);mySerial.print(c_tw);delay(3000);mySerial.write(12);break; // s - Number of occupied spaces
     default:Serial.println("Unclear command for display");break;
   }
 }
@@ -157,24 +161,24 @@ void loop() {
     localfinish=false;
     int c_itsc=0;
     bool isintersection=false;
-    unsigned long tic, toc;
+    unsigned long tic1, toc1, tic2, toc2, tic_size1, toc_size1, tic_size2, toc_size2;
     while(!localfinish){
       //move the robot
       isintersection=!rob.linefollowing();
       if(isintersection && c_itsc==0) { //if meet intersection
         rob.drive('f');delay(500); 
         rob.drive('r');delay(oneeighty); //turn 180
-        tic=millis();
+        tic1=millis();
         c_itsc++;
       }
       else if(isintersection && c_itsc==2){
         rob.drive('f');delay(500);
-        rob.drive('r');delay(oneeighty+100);//turn 180
+        rob.drive('r');delay(oneeighty);//turn 180
         c_itsc++;parkidx++;
       }
       else if(isintersection && c_itsc==1){
-        rob.drive('f');delay(600);//pass the intersection
-        c_itsc++;parkidx++;tic=millis();
+        rob.drive('f');delay(1000);//pass the intersection
+        c_itsc++;parkidx++;tic2=millis();
       }
       else if(isintersection && c_itsc==3){
         rob.drive('f');delay(500);
@@ -186,32 +190,35 @@ void loop() {
       if(!isintersection && c_itsc==1 && parkinglot[parkidx]==0){
         bool isobj=rob.isobstacle();
         if(isobj==true){
-          toc=millis();
-          rob.mySerial.write(12); // Clear
-          rob.mySerial.write(17);
-          delay(5); // Required delay
-          rob.mySerial.print(toc-tic);
-          if(toc-tic<200){parkinglot[parkidx]=2;}
+          obstacles_no++;
+          toc1=millis();
+          //rob.mySerial.write(12); // Clear
+          //rob.mySerial.write(17);
+          //delay(5); // Required delay
+          //rob.mySerial.print(toc1-tic1);
+          if(toc1-tic1<300){parkinglot[parkidx]=2;c_tw++;}
+          
           else{
-            tic=millis();
-            while(rob.isobstacle()){rob.linefollowing();}
-            toc=millis();
-            if(toc-tic>200)
-              parkinglot[parkidx]=1;
+            tic_size1=millis();
+            while(rob.isobstacle()){if(!rob.linefollowing())break;}
+            toc_size1=millis();
+            parkinglot[parkidx]=1;
+            if(toc_size1-tic_size1<1500) {c_tw++;}
           }
         }
       }
       if(!isintersection && c_itsc==2 && parkinglot[parkidx]==0){
         bool isobj=rob.isobstacle();
         if(isobj==true){
-          toc=millis();
-          if(toc-tic>2500){parkinglot[parkidx]=2;}
+          obstacles_no++;
+          toc2=millis();
+          if(toc2-tic2>2000){parkinglot[parkidx]=2;}
           else{
-            tic=millis();
+            tic_size2=millis();
             while(rob.isobstacle()){if(!rob.linefollowing()){break;}}
-            toc=millis(); 
-            if(toc-tic>200 && parkinglot[parkidx]==0)
-              parkinglot[parkidx]=1;
+            toc_size2=millis(); 
+            if(toc_size2-tic_size2>100 && parkinglot[parkidx]==0) {
+              parkinglot[parkidx]=1;}
         }
         }
       }
@@ -234,6 +241,8 @@ void loop() {
           rob.drive('s');delay(500);
           rob.lcd_display('p');
           rob.lcd_display('s');
+          rob.lcd_display('f');
+          rob.lcd_display('t');
           finish=true;localfinish=true;
         }
         else if(parkinglot[parkidx-1]==0||parkinglot[parkidx-1]==2){
@@ -243,6 +252,8 @@ void loop() {
           rob.drive('s');delay(500);
           rob.lcd_display('p');
           rob.lcd_display('s');
+          rob.lcd_display('f');
+          rob.lcd_display('t');
           finish=true;localfinish=true;
         }
         else{
